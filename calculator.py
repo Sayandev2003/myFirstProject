6373acf1645e6144fdb96c8e1e91512ff743034e
Sayandev2003 <sayandevbiswas0@gmail.com>
2026-06-05 00:03:24 +0530

    ADDED a calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+print("1. Add")
+print("2. Substruct")
+print("3. Multiply")
+print("4. Divide")
+print("5. Power multiply")
+
+choice = int(input("Choose an option: "))
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+if choice == 1:
+    print("Result = ", num1 + num2)
+elif choice == 2:
+    print("Result =", num1 - num2)
+elif choice == 3:
+    print("Result = ", num1 * num2)
+elif choice == 4:
+    print("Result = ", num1 / num2)
+elif choice == 5:
+    print("Result = ", num1 ** num2)
+else:
+    print("Invalid option")
