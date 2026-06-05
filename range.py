71aca96e6319d7b25cf8f94a262f56fd06d6019d
Sayandev2003 <sayandevbiswas0@gmail.com>
2026-06-05 15:20:49 +0530

    Added a Multiplication table

---
+++ range.py
@@ -0,0 +1,20 @@
+#example 1
+
+for i in range(5):
+    print("HELLO SAYAN")
+
+#example 2
+
+for o in range(6):
+    print(o)
+
+#example 3
+
+for x in range(-8, 9):
+    print (x)
+
+#example
+
+for t in range(1, 11):
+    print(t)
+
